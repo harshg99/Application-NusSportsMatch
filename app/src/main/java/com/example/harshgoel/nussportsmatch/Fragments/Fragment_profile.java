5e09577bcc2ce788c6978b875d55b20fd84f20d5
mercury070599 <hgoel1000p@gmail.com>
2017-07-20 16:39:15 +0530

    The project

---
+++ app/src/main/java/com/example/harshgoel/nussportsmatch/Fragments/Fragment_profile.java
@@ -0,0 +1,159 @@
+package com.example.harshgoel.nussportsmatch.Fragments;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.harshgoel.nussportsmatch.Logic.Player;
+import com.example.harshgoel.nussportsmatch.Logic.sportsPlayer;
+import com.example.harshgoel.nussportsmatch.ProfileDataPackage.EditProfile;
+import com.example.harshgoel.nussportsmatch.ProfileDataPackage.handlephoto;
+import com.example.harshgoel.nussportsmatch.ProfileDataPackage.setSports;
+import com.example.harshgoel.nussportsmatch.R;
+import com.example.harshgoel.nussportsmatch.SignIn;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Harsh Goel on 7/17/2017.
+ */
+
+public class Fragment_profile extends Fragment {
+    public ImageView profilephoto;
+    public Button addphoto;
+    public TextView profilename;
+    public Button editprofile;
+    public Button setsports;
+    public Button Logout;
+    public Player userplayer;
+    public Button questionnaire;
+    public static final int photo=10;
+    private FirebaseAuth auth;
+    private DatabaseReference userref;
+    public Button plays;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View fragment_profile =inflater.inflate(R.layout.activity_profile,container,false);
+        profilephoto=(ImageView)fragment_profile.findViewById(R.id.ProfilePhoto);
+        auth=FirebaseAuth.getInstance();
+        profilename=(TextView)fragment_profile.findViewById(R.id.Name);
+        userref= FirebaseDatabase.getInstance().getReference();
+        userref=userref.child("users").child(auth.getCurrentUser().getUid());
+        userref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                userplayer=dataSnapshot.getValue(Player.class);
+                profilename.setText(userplayer.getName());
+                Log.d("UserPlayer:",userplayer.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Database Reference:","Read ERROR");
+            }
+        });
+        addphoto=(Button)fragment_profile.findViewById(R.id.addphoto);
+        editprofile=(Button)fragment_profile.findViewById(R.id.edit_profile);
+        setsports=(Button)fragment_profile.findViewById(R.id.selectsports);
+        Logout=(Button)fragment_profile.findViewById(R.id.logout);
+        plays=(Button)fragment_profile.findViewById(R.id.Plays);
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                getActivity().finish();
+                Intent intent=new Intent()
+                        .setClass(getActivity(),SignIn.class);
+                startActivity(intent);
+            }
+        });
+        addphoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    getActivity().finish();
+                    Intent intent=new Intent()
+                            .setClass(getActivity(),handlephoto.class);
+                    startActivity(intent);
+
+            }
+        });
+        editprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+                Intent intent=new Intent()
+                        .setClass(getActivity(), EditProfile.class);
+                startActivity(intent);
+
+            }
+        });
+        setsports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+                Intent intent=new Intent()
+                        .setClass(getActivity(), setSports.class);
+                startActivity(intent);
+            }
+        });
+        plays.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<CharSequence>sportsel=new ArrayList<CharSequence>();
+                if(userplayer.getTennis().getisAdded()) {
+                    sportsel.add("Tennis");
+                }
+                if(userplayer.getSquash().getisAdded()) {
+                    sportsel.add("Squash");
+                }
+                if(userplayer.getBadminton().getisAdded()) {
+                    sportsel.add("Badminton");
+                }
+                if(userplayer.getTt().getisAdded()) {
+                    sportsel.add("Table Tennis");
+                }
+                CharSequence[] sports=sportsel.toArray(new CharSequence[sportsel.size()]);
+                AlertDialog.Builder SelectSports=new AlertDialog.Builder(getActivity());
+                SelectSports.setTitle("Select sports")
+                        .setItems(sports, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                    }
+                });
+                SelectSports.create().show();
+            }
+        });
+        return fragment_profile;
+    }
+
+}
---
+++ app/src/main/java/com/example/harshgoel/nussportsmatch/ProfileDataPackage/setSports.java
@@ -0,0 +1,126 @@
+package com.example.harshgoel.nussportsmatch.ProfileDataPackage;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.example.harshgoel.nussportsmatch.AppLoginPage;
+import com.example.harshgoel.nussportsmatch.Logic.Player;
+import com.example.harshgoel.nussportsmatch.Logic.sportsPlayer;
+import com.example.harshgoel.nussportsmatch.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Harsh Goel on 7/18/2017.
+ */
+
+public class setSports extends AppCompatActivity {
+
+    public CheckBox tennisbox;
+    public CheckBox Badmintonbox;
+    public CheckBox squashbox;
+    public CheckBox ttbox;
+    public Button confirmBUT;
+    private FirebaseAuth auth;
+    private DatabaseReference userref;
+    public Player userplayer;
+    private int tenniscomp;
+    private int ttcomp;
+    private int badcomp;
+    private int squashcomp;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_sports);
+        confirmBUT=(Button)findViewById(R.id.Confirm);
+        tennisbox=(CheckBox)findViewById(R.id.Tennis);
+        Badmintonbox=(CheckBox)findViewById(R.id.badminton);
+        ttbox=(CheckBox)findViewById(R.id.TableTennis);
+        tenniscomp=0;
+        ttcomp=0;
+        badcomp=0;
+        squashcomp=0;
+        squashbox=(CheckBox)findViewById(R.id.Squash);
+        auth=FirebaseAuth.getInstance();
+        userref= FirebaseDatabase.getInstance().getReference();
+        userref=userref.child("users").child(auth.getCurrentUser().getUid());
+        userref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                userplayer=dataSnapshot.getValue(Player.class);
+                Log.d("UserPlayer:",userplayer.toString());
+                if(userplayer.getTennis().getisAdded()){
+                    tennisbox.setChecked(true);
+                }
+                if(userplayer.getSquash().getisAdded()){
+                    squashbox.setChecked(true);
+                }
+                if(userplayer.getBadminton().getisAdded()){
+                    Badmintonbox.setChecked(true);
+                }
+                if(userplayer.getTt().getisAdded()){
+                    ttbox.setChecked(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Database Reference:","Read ERROR");
+            }
+        });
+
+    }
+    public void ConfirmSports(View v) {
+        if (tennisbox.isChecked())
+            userplayer.getTennis().setAdded(true);
+        else
+            userplayer.getTennis().setAdded(false);
+        if (squashbox.isChecked())
+            userplayer.getSquash().setAdded(true);
+        else
+            userplayer.getSquash().setAdded(false);
+        if (ttbox.isChecked())
+            userplayer.getTt().setAdded(true);
+        else
+            userplayer.getTt().setAdded(false);
+        if (Badmintonbox.isChecked())
+            userplayer.getBadminton().setAdded(true);
+        else
+            userplayer.getBadminton().setAdded(false);
+
+        userref.child("tennis").child("isAdded").setValue(userplayer.getTennis().getisAdded());
+        userref.child("squash").child("isAdded").setValue(userplayer.getSquash().getisAdded());
+        userref.child("badminton").child("isAdded").setValue(userplayer.getBadminton().getisAdded());
+        userref.child("tt").child("isAdded").setValue(userplayer.getTt().getisAdded());
+        if ((!userplayer.getTennis().isQuestionaireCompleted()&&userplayer.getTennis().getisAdded())
+                || (!userplayer.getBadminton().isQuestionaireCompleted()&&userplayer.getBadminton().getisAdded())
+                || (!userplayer.getSquash().isQuestionaireCompleted()&&userplayer.getSquash().getisAdded())
+                || (!userplayer.getTt().isQuestionaireCompleted()&&userplayer.getTt().getisAdded())) {
+            this.finish();
+            Intent intent = new Intent().setClass(setSports.this, Questionaire.class);
+            startActivity(intent);
+        } else {
+            this.finish();
+            Intent intent = new Intent().setClass(this, AppLoginPage.class);
+            startActivity(intent);
+        }
+    }
+
+}
